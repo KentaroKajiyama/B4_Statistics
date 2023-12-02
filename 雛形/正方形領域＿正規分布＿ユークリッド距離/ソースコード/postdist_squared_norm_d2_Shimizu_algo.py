a8643b8c10009dfe16267b2cd50690d60edf6b53
Kentaro Kajiyama <kemushi555@icloud.com>
2023-12-02 18:09:39 +0900

    geometricmedianの追加

--- 雛形/正方形領域＿正規分布＿ユークリッド距離/ソースコード/postdist_squared_norm_d2_Shimizu_algo.py
+++ 雛形/正方形領域＿正規分布＿ユークリッド距離/ソースコード/postdist_squared_norm_d2_Shimizu_algo.py
@@ -169,6 +169,36 @@ def g_function(pnts,i,xy_index):
         sigma += tmp_sigma_upper/tmp_sigma_lower
     return sigma
 
+#geometric medianの計算
+
+def geometric_median(X, eps=1e-5):
+    y = np.mean(X, 0)
+
+    while True:
+        D = cdist(X, [y])
+        nonzeros = (D != 0)[:, 0]
+
+        Dinv = 1 / D[nonzeros]
+        Dinvs = np.sum(Dinv)
+        W = Dinv / Dinvs
+        T = np.sum(W * X[nonzeros], 0)
+
+        num_zeros = len(X) - np.sum(nonzeros)
+        if num_zeros == 0:
+            y1 = T
+        elif num_zeros == len(X):
+            return y
+        else:
+            R = (T - y) * Dinvs
+            r = np.linalg.norm(R)
+            rinv = 0 if r == 0 else num_zeros/r
+            y1 = max(0, 1-rinv)*T + min(1, rinv)*y
+
+        if euclidean(y, y1) < eps:
+            return y1
+
+        y = y1
+
 if __name__ == '__main__':
     main()
     
